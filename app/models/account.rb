525af529887b4b04fecbcfb2e2837344a2f02c2b
Will Ricketts <will@willricketts.com>
2016-04-20 19:23:13 -0400

    added uniqueness constraints to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,6 @@ class Account
   belongs_to :user
   has_many :characters
 
-  field :key_id, :type => String
-  field :vcode, :type => String
+  field :key_id, :type => String, :uniq => true, :required => true
+  field :vcode, :type => String, :uniq => true, :required => true
 end
